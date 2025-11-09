701221492854ccf554087f28f8a808e84012a7da
lionelladrog <36386155+lionelladrog@users.noreply.github.com>
2025-11-09 15:22:33 +0400

    test backend

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 import LoginForm from "@/components/LoginForm";
-// import TestDbPage from "../test-db/page";
 
 function Login() {
   return (
     <div className="min-h-screen  bg-gradient-to-br from-cyan-50 via-blue-50 to-slate-100 p-4">
-      {/* <TestDbPage /> */}
       <LoginForm />
     </div>
   );
